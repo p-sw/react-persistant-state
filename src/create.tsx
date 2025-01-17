8b14a42b573b157b1852924ed2b27ac4785130f9
Shinwoo PARK <shinwoo.park@psw.kr>
2025-01-17 11:22:00 +0900

    docs: add comments on create.tsx

--- src/create.tsx
+++ src/create.tsx
@@ -1,7 +1,6 @@
 import {
   Dispatch,
   SetStateAction,
-  useEffect,
   useMemo,
   useSyncExternalStore,
 } from "react";
@@ -11,10 +10,36 @@ type TNotExists = typeof NotExists;
 
 export function createPersistantStateStore() {
   type Listener = () => void;
+  /**
+   * object for storing persistant states by id
+   *
+   * @example
+   * // This is how inside looks like
+   * store = {
+   *   state1: {
+   *     value1: "value",
+   *     form: {
+   *       input1: "value",
+   *       input2: 2,
+   *       input3: false,
+   *     },
+   *   },
+   *   state2: {
+   *     // ...
+   *   }
+   * }
+   */
   const store: Record<string, unknown> = {};
+  
   const listeners: Record<string, Listener[]> = {};
   let storeListeners: Listener[] = [];
 
+  /**
+   * Register a listener to detect change of store
+   *
+   * @internal
+   * @param listener
+   */
   function subscribeStore(listener: Listener) {
     storeListeners.push(listener);
     return () => {
@@ -22,6 +47,13 @@ export function createPersistantStateStore() {
     };
   }
 
+  /**
+   * Register a listener to detect change of state
+   *
+   * @internal
+   * @param stateId
+   * @param listener
+   */
   function subscribeState(stateId: string, listener: Listener) {
     if (!(stateId in listeners)) listeners[stateId] = [];
     listeners[stateId].push(listener);
@@ -30,17 +62,44 @@ export function createPersistantStateStore() {
     };
   }
 
+  /**
+   * Get snapshot of state
+   *
+   * @internal
+   * @param stateId
+   */
   function getStateSnapshot(
     stateId: string,
   ): (typeof store)[string] | TNotExists {
+    /** Reason why it returns `NotExists` symbol instead of undefined
+     * There's two possibility of `store[stateId]` is undefined:
+     * 1. store does not have item with key `stateId` (in this case we have to set default value)
+     * 2. intentionally set `store[stateId]` as undefined, to represent empty value
+     * To separate this two case, it returns NotExists symbol when there's no `stateId` key in `store`
+     */
     if (!(stateId in store)) return NotExists;
     return store[stateId];
   }
 
+  /**
+   * Notify listeners when state changed
+   *
+   * Should be called on state change
+   *
+   * @internal
+   * @param stateId
+   */
   function notifyState(stateId: string) {
     (listeners[stateId] ?? []).forEach((l) => l());
   }
 
+  /**
+   * Build and returns setState for `stateId`.
+   *
+   * Returned setState should act like normal setState on useState.
+   *
+   * @param stateId
+   */
   function setStateBuilder<T>(stateId: string): Dispatch<SetStateAction<T>> {
     return (valueOrUpdator) => {
       if (typeof valueOrUpdator === "function") {
@@ -54,6 +113,13 @@ export function createPersistantStateStore() {
     };
   }
 
+  /**
+   * useState simulator for `store[stateId]`
+   *
+   * @internal
+   * @param stateId
+   * @param defaultValue
+   */
   function useState<T = undefined>(
     stateId: string,
     defaultValue?: T | (() => T),
@@ -76,15 +142,25 @@ export function createPersistantStateStore() {
       return snapshot;
     }, [snapshot, defaultValue, stateId]);
 
-    useEffect(() => {}, [snapshot, defaultValue, stateId]);
     return [afterSnapshot, setStateBuilder(stateId)];
   }
 
+  /**
+   * Completely delete stateId from store and listeners
+   *
+   * @param stateId
+   */
   function deleteState(stateId: string) {
     delete store[stateId];
     delete listeners[stateId];
   }
 
+  /**
+   * Use entire store instead of single state.
+   *
+   * You **SHOULD** use setStateBuilder to change value of state
+   * to notify subscribers that state changed and you should rerender.
+   */
   function useStore(): [typeof store, typeof setStateBuilder, typeof deleteState] {
     return [useSyncExternalStore(subscribeStore, () => store), setStateBuilder, deleteState]
   }
